9623ff30d9e5961256c56c624283fb5776633ed7
z46689944-beep <z46689944@gmail.com>
2026-09-19 10:30:41 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yar-keshavarz-shell-v9';
+const CACHE='yar-keshavarz-shell-v12';
 const CORE=['./','./index.html','./offline/offline-ai.js','./offline/agriculture-db.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo.png','./wheat-hero.jpg','./admin.html','./admin.js','./admin.css','./knowledge/knowledge.json','./presence.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
